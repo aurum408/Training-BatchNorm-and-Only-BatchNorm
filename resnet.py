98ad3d814d32dc3d13111825d52f0f1669cd32d2
anastasia <ak@braingarden.ai>
2020-12-14 18:34:08 +0300

    Initial commit

---
+++ resnet.py
@@ -0,0 +1,249 @@
+from typing import Union, List
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
+
+
+class TrainableParam:
+    def __init__(self, shape: tuple, mode: str, name: str) -> None:
+        self.name = name
+        self.shape = shape
+        self.mode = mode
+
+    def initialize_param(self) -> nn.Parameter:
+        if self.mode == "zeros":
+            x = torch.zeros(self.shape)
+        elif self.mode == "ones":
+            x = torch.ones(self.shape)
+        elif self.mode == "uniform":
+            x = torch.rand(self.shape)
+        else:
+            raise NotImplementedError
+        x = nn.Parameter(x)
+        return x
+
+
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    # http://d2l.ai/chapter_convolutional-modern/batch-norm.html
+    # Use `is_grad_enabled` to determine whether the current mode is training
+    # mode or prediction mode
+    if not torch.is_grad_enabled():
+        # If it is prediction mode, directly use the mean and variance
+        # obtained by moving average
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        assert len(X.shape) in (2, 4)
+        if len(X.shape) == 2:
+            # When using a fully-connected layer, calculate the mean and
+            # variance on the feature dimension
+            mean = X.mean(dim=0)
+            var = ((X - mean) ** 2).mean(dim=0)
+        else:
+            # When using a two-dimensional convolutional layer, calculate the
+            # mean and variance on the channel dimension (axis=1). Here we
+            # need to maintain the shape of `X`, so that the broadcasting
+            # operation can be carried out later
+            mean = X.mean(dim=(0, 2, 3), keepdim=True)
+            var = ((X - mean) ** 2).mean(dim=(0, 2, 3), keepdim=True)
+        # In training mode, the current mean and variance are used for the
+        # standardization
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        # Update the mean and variance using moving average
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # Scale and shift
+    return Y, moving_mean.data, moving_var.data
+
+
+class BatchNorm(nn.Module):
+    # `num_features`: the number of outputs for a fully-connected layer
+    # or the number of output channels for a convolutional layer. `num_dims`:
+    # 2 for a fully-connected layer and 4 for a convolutional layer
+    def __init__(self, num_features: int, num_dims: int, gamma: str = "uniform",
+                 beta: str = "zeros", eps: float = 1e-5, momentum: float = 0.9) -> None:
+        super().__init__()
+        if num_dims == 2:
+            shape = (1, num_features)
+        else:
+            shape = (1, num_features, 1, 1)
+        # The scale parameter and the shift parameter (model parameters) are
+        # initialized to uniform sampling from [0, 1) and 0, respectively
+        self.gamma = TrainableParam(shape, gamma, name="gamma").initialize_param()
+        self.beta = TrainableParam(shape, beta, name="beta").initialize_param()
+        # The variables that are not model parameters are initialized to 0
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.zeros(shape)
+        self.eps = eps
+        self.momentum = momentum
+
+    def forward(self, X):
+        # If `X` is not on the main memory, copy `moving_mean` and
+        # `moving_var` to the device where `X` is located
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        # Save the updated `moving_mean` and `moving_var`
+        Y, self.moving_mean, self.moving_var = batch_norm(
+            X, self.gamma, self.beta, self.moving_mean,
+            self.moving_var, self.eps, self.momentum)
+        return Y
+
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    #print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = BatchNorm(planes, 4)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = BatchNorm(planes, 4)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = LambdaLayer(lambda x:
+                                        F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = BatchNorm(16, 4)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def resnet20():
+    return ResNet(BasicBlock, [3, 3, 3])
+
+
+def resnet32():
+    return ResNet(BasicBlock, [5, 5, 5])
+
+
+def resnet44():
+    return ResNet(BasicBlock, [7, 7, 7])
+
+
+def resnet56():
+    return ResNet(BasicBlock, [9, 9, 9])
+
+
+def resnet110():
+    return ResNet(BasicBlock, [18, 18, 18])
+
+
+def resnet1202():
+    return ResNet(BasicBlock, [200, 200, 200])
+
+
+def name_in_list(name: str, lst: List) -> bool:
+    for item in lst:
+        if item in name:
+            return True
+    return False
+
+
+def freeze_model(model: nn.Module, param_names: Union[List[str], None] = None) -> None:
+    for name, param in model.named_parameters():
+        if param_names:
+            if name_in_list(name, param_names):
+                param.requires_grad = False
+        else:
+            param.requires_grad = False
+
+
+def unfreeze_model(model: nn.Module, param_names: Union[List[str], None] = None) -> None:
+    for name, param in model.named_parameters():
+        if param_names:
+            if name_in_list(name, param_names):
+                param.requires_grad = True
+        else:
+            param.requires_grad = True
+
+
+def test(net):
+    import numpy as np
+    total_params = 0
+
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    print("Total number of params", total_params)
+    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size()) > 1, net.parameters()))))
+
+
+if __name__ == "__main__":
+
+    #https://github.com/akamaster/pytorch_resnet_cifar10/blob/master/resnet.py
+
+    net = resnet20()
+    freeze_model(net)
+    test(net)
+
+    unfreeze_model(net)
+    test(net)
+
+    freeze_model(net)
+    unfreeze_model(net, param_names=["gamma", "beta"])
+    test(net)
+    # for name, param in net.named_parameters():
+    #     print(name)
+    #
+    # for net_name in __all__:
+    #     if net_name.startswith('resnet'):
+    #         print(net_name)
+    #         test(globals()[net_name]())
+    #         print()
