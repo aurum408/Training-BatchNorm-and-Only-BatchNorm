853f44e68b7d28eb268e179d71614de99d2e3e25
anastasia <ak@braingarden.ai>
2020-12-16 14:21:11 +0300

    add tmp folder with plots

--- logparser.py
+++ logparser.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib
 from matplotlib import pyplot as plt
+from typing import List
 
 names = ["Test", "Epoch", "Time", "Data", "Loss", "Prec"]
 
@@ -34,7 +35,7 @@ def parse_metric(metric: str) -> dict:
 def parse_line(line: str) -> dict:
     metrics = line.split("\t")
     data = [parse_metric(metric) for metric in metrics]
-    data = { k:v for i in data for k, v in i.items()}
+    data = {k: v for i in data for k, v in i.items()}
     return data
 
 
@@ -81,12 +82,80 @@ def parse_log(fpath: str) -> dict:
 
     return info
 
+def _plot(data: list, label: str, color: str) -> None:
+    plt.plot(data, color, label=label)
+
+
+def plot(configs: list, xlabel: str, ylabel: str,
+         title: str, legend: str, fname: str) -> None:
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+
+    # plt.figure()
+    plt.title(title)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.7, box.height])
+    for cfg in configs:
+        _plot(*cfg)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=legend)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid()
+    plt.savefig("tmp/{}.png".format(fname))
+
 
 if __name__ == '__main__':
-    fpath_SGDbn = "/Users/anastasia/Downloads/resnet32SGDbn_Dec-15-2020 (3).txt"
-    fpath_SGDall = "/Users/anastasia/Downloads/resnet32SGDall_Dec-15-2020 (5).txt"
-    fpath_LBFGSall = "/Users/anastasia/Downloads/resnet32LBFGSall_Dec-15-2020.txt"
-    fpath_LBFGSbn = "/Users/anastasia/Downloads/resnet32LBFGSbn_Dec-15-2020 (2).txt"
+    SGDbn = parse_log("/Users/anastasia/Downloads/resnet32SGDbn_Dec-15-2020 (3).txt")
+    SGDall = parse_log("/Users/anastasia/Downloads/resnet32SGDall_Dec-15-2020 (5).txt")
 
-    data = [parse_log(fpath) for fpath in [fpath_SGDbn, fpath_SGDall, fpath_LBFGSbn, fpath_LBFGSall]]
+    LBFGSall = parse_log("/Users/anastasia/Downloads/resnet32LBFGSall_Dec-15-2020 (3).txt")
+    LBFGSbn = parse_log("/Users/anastasia/Downloads/resnet32LBFGSbn_Dec-15-2020 (8).txt")
+
+    plot([[SGDbn["train_loss"][1:], "SGD_bn", "r"],
+          [SGDall["train_loss"][1:], "SGD_all", "b"],
+          [LBFGSbn["train_loss"][1:], "LBFGS_bn", "g"],
+          [LBFGSall["train_loss"][1:], "LBFGS_all", "y"]],
+         "epoch", "loss", "train_loss", "best", "train_loss")
+
+    plot([[SGDbn["train_loss"][1:], "SGD_bn_train", "r"],
+          [SGDall["train_loss"][1:], "SGD_all_train", "b"],
+          [LBFGSbn["train_loss"][1:], "LBFGS_bn_train", "g"],
+          [LBFGSall["train_loss"][1:], "LBFGS_all_train", "y"],
+          [SGDbn["val_loss"][1:], "SGD_bn_val", "purple"],
+          [SGDall["val_loss"][1:], "SGD_all_val", "orange"],
+          [LBFGSbn["val_loss"][1:], "LBFGS_bn_val", "pink"],
+          [LBFGSall["val_loss"][1:], "LBFGS_all_val", "olive"]],
+         "epoch", "loss", "loss", "best", "loss")
+
+    plot([[SGDbn["val_loss"][1:], "SGD_bn", "r"],
+          [SGDall["val_loss"][1:], "SGD_all", "b"],
+          [LBFGSbn["val_loss"][1:], "LBFGS_bn", "g"],
+          [LBFGSall["val_loss"][1:], "LBFGS_all", "y"]],
+         "epoch", "loss", "val_loss", "best", "val_loss")
+
+    plot([[SGDbn["train_acc"][1:], "SGD_bn", "r"],
+          [SGDall["train_acc"][1:], "SGD_all", "b"],
+          [LBFGSbn["train_acc"][1:], "LBFGS_bn", "g"],
+          [LBFGSall["train_acc"][1:], "LBFGS_all", "y"]],
+         "epoch", "accuracy", "train_acc", "best", "train_acc")
+
+    plot([[SGDbn["train_acc"][1:], "SGD_bn_train", "r"],
+          [SGDall["train_acc"][1:], "SGD_all_train", "b"],
+          [LBFGSbn["train_acc"][1:], "LBFGS_bn_train", "g"],
+          [LBFGSall["train_acc"][1:], "LBFGS_all_train", "y"],
+          [SGDbn["val_acc"][1:], "SGD_bn_val", "purple"],
+          [SGDall["val_acc"][1:], "SGD_all_val", "orange"],
+          [LBFGSbn["val_acc"][1:], "LBFGS_bn_val", "pink"],
+          [LBFGSall["val_acc"][1:], "LBFGS_all_val", "olive"]],
+         "epoch", "accuracy", "accuracy", "best", "acc")
+
+    plot([[SGDbn["val_acc"][1:], "SGD_bn", "r"],
+          [SGDall["val_acc"][1:], "SGD_all", "b"],
+          [LBFGSbn["val_acc"][1:], "LBFGS_bn", "g"],
+          [LBFGSall["val_acc"][1:], "LBFGS_all", "y"]],
+         "epoch", "accuracy", "val_acc", "best", "val_acc")
+
+
+
     print("ok")
